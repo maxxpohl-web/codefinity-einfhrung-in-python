6403140208a9a85df0dbb3c6a94a16a23cf63389
maxxpohl-web <>
2026-02-05 15:53:38 +0000

    Variable assignment

--- variablen_und_typen/variable_assignment/main.py
+++ variablen_und_typen/variable_assignment/main.py
@@ -1,5 +1,12 @@
 
-
+#Variable (String)
+item_name = "Eggs"
+#Variable (float)
+item_price= 3.99 
+#Variable (Integer)
+item_stock_quantity = 120
+#Variable (Boolean)
+in_stock= True 
 
 # Testing
 print("Item : ", item_name)
