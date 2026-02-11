0a01a70c33f9704e319e8ccd21a2a9600ac46e39
maxxpohl-web <>
2026-02-11 16:48:52 +0000

    Herausforderung: Product-Management-Capstone

---
+++ bedingte_anweisungen/herausforderung_product_management_capstone/main.py
@@ -0,0 +1,21 @@
+# Input variables
+days_until_expiration = 5  # Example value
+stock_level = 60 # Example value
+product_type = "Perishable"  # Can be "Perishable" or "Non-Perishable"
+
+product_Perishable = "Perishable" in product_type # prüfen ob das Produk verderblich ist 
+# print(product_Perishable) 
+
+if product_type == "Perishable":
+    if days_until_expiration == 3 or days_until_expiration <= 3 and stock_level > 50:
+       print("30% discount applied")
+
+    elif 4 <= days_until_expiration <= 6 and stock_level > 50:
+       print("20% discount applied")
+
+    elif days_until_expiration > 6 and stock_level <= 50: 
+       print("10% discount applied")
+
+else:
+    print ("No discount available for non-perishable items. ")
+
