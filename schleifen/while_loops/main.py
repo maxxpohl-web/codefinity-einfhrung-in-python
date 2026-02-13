e80a030098baf5fc47c6cf5fb1608d70e84f7686
maxxpohl-web <>
2026-02-13 15:08:13 +0000

    While Loops

---
+++ schleifen/while_loops/main.py
@@ -0,0 +1,11 @@
+start_number = 5
+countdown_values = []
+
+current = 5
+
+while current >= 1:
+    countdown_values.append(current)
+    current = current -1
+
+print("Discount countdown complete!")
+print(countdown_values)
